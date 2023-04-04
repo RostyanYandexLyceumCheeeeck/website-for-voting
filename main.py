99607717d5d31799c79ab3017299eb615c184f71
Cwerti <cwerti84@gmail.com>
2023-04-04 21:32:33 +0300

    сделал страницу ввода изображения

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import os
 import time
 import random
 
-from flask import Flask, render_template, redirect, make_response, jsonify
+from flask import Flask, render_template, redirect, make_response, jsonify, request, session
 from dotenv import load_dotenv
 from flask_login import login_user, LoginManager
 from flask_restful import Api
+from wtforms import MultipleFileField, SubmitField
+from flask_wtf import FlaskForm
 
 from data import db_session
 from data.__all_models import User
@@ -26,6 +28,11 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+class ImageForm(FlaskForm):
+    images = MultipleFileField('Select Images')
+    submit = SubmitField('Submit')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def start_screan():
     return render_template('start_scr.html')
@@ -33,7 +40,43 @@ def start_screan():
 
 @app.route('/create', methods=['GET', 'POST'])
 def start_scre1an():
-    return render_template('create.html')
+    test_name = request.args.get('test-name')
+    test_description = request.args.get('test-description')
+    test_type = request.args.get('test-type')
+    test_kol = request.args.get('test-kol')
+    test_difficulty = request.args.get('test-difficulty')
+    if test_kol is None:
+        return render_template('create.html', flag=False)
+    else:
+        try:
+            test_kol = int(test_kol)
+        except Exception:
+            return render_template('create.html', flag=True)
+        data = {}
+        data['test_name'] = test_name
+        data['test_description'] = test_description
+        data['test_type'] = test_type
+        data['test_kol'] = test_kol
+        data['test_difficulty'] = test_difficulty
+        session['img'] = data
+        return redirect('/img')
+
+
+@app.route('/img', methods=['GET', 'POST'])
+def upload_images():
+    form = ImageForm()
+    kol_img = session.get('img', None)['test_kol']
+    if form.validate_on_submit():
+        ls_is_file = [os.path.isfile(image.filename) for image in form.images.data]
+        ls_is_img = [not x for x in ls_is_file]
+        if all(ls_is_img) and len(form.images.data) == \
+                kol_img:
+            for image in form.images.data:
+                image.save(image.filename)
+            return redirect("/")
+        else:
+            return render_template('img.html', form=form, isimg=True, kol=kol_img)
+    return render_template('img.html', form=form, isimg=False, kol=kol_img)
 
 
 @login_manager.user_loader
