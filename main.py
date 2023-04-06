6a3b4819914085fda8768f6915306a00c7ec31fc
Cwerti <cwerti84@gmail.com>
2023-04-06 13:20:47 +0300

    Финальная версия создания тестов

--- main.py
+++ main.py
@@ -40,43 +40,104 @@ def start_screan():
 
 @app.route('/create', methods=['GET', 'POST'])
 def start_scre1an():
-    test_name = request.args.get('test-name')
-    test_description = request.args.get('test-description')
-    test_type = request.args.get('test-type')
-    test_kol = request.args.get('test-kol')
-    test_difficulty = request.args.get('test-difficulty')
+    form = ImageForm()
+    test_name = request.form.get('test-name')
+    test_description = request.form.get('test-description')
+    test_type = request.form.get('test-type')
+    test_kol = request.form.get('test-kol')
     if test_kol is None:
-        return render_template('create.html', flag=False)
+        return render_template('create.html', flag=False, form=form, file=False)
     else:
         try:
             test_kol = int(test_kol)
         except Exception:
-            return render_template('create.html', flag=True)
+            return render_template('create.html', flag=True, form=form, file=False)
+        if len(form.images.data) != 1:
+            return render_template('create.html', flag=True, form=form, file=True)
+        filename = form.images.data[0].filename
+        file = form.images.data[0]
+        if filename.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif', '.webp', '.jfif')):
+            path = os.path.join('static/images', filename)
+            file.save(path)
+        else:
+            return render_template('create.html', flag=True, form=form, file=True)
         data = {}
         data['test_name'] = test_name
         data['test_description'] = test_description
         data['test_type'] = test_type
         data['test_kol'] = test_kol
-        data['test_difficulty'] = test_difficulty
+        data['preview'] = path
         session['img'] = data
+        test = {"head": data, 'question': []}
+        session['test'] = test
         return redirect('/img')
 
 
 @app.route('/img', methods=['GET', 'POST'])
 def upload_images():
     form = ImageForm()
+    test = session.get('test', None)
+    session['test'] = test
     kol_img = session.get('img', None)['test_kol']
     if form.validate_on_submit():
+        ls_name = [image.filename for image in form.images.data]
+        session['img_name'] = ls_name
         ls_is_file = [os.path.isfile(image.filename) for image in form.images.data]
         ls_is_img = [not x for x in ls_is_file]
         if all(ls_is_img) and len(form.images.data) == \
                 kol_img:
             for image in form.images.data:
-                image.save(image.filename)
-            return redirect("/")
+                filname = image.filename
+                path = os.path.join('static/images', filname)
+                image.save(path)
+            return redirect("/add")
         else:
             return render_template('img.html', form=form, isimg=True, kol=kol_img)
     return render_template('img.html', form=form, isimg=False, kol=kol_img)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_text_question():
+    name_img = session.get('img_name', None)
+    message = ''
+    if request.method == 'POST':
+        if request.form.get('next') is None:
+            question = request.form.get('question')
+            description = request.form.get('description')
+            id_ = request.form.get('id')
+            que = {}
+            que['question'] = question
+            que['description'] = description
+            content = {'id': id_, 'data': que}
+            test = session.get('test', None)
+            data = test['question']
+            asd = list(filter(lambda x: x['id'] == id_, data))
+            if len(asd) == 0:
+                test['question'].append(content)
+            else:
+                ind = test['question'].index(*asd)
+                test['question'][ind] = content
+            session['test'] = test
+        else:
+            test = session.get('test', None)
+            data = test['question']
+            print(data)
+            id = list(map(int, list(i['id'] for i in data)))
+            need_id = []
+            for i in range(len(name_img)):
+                if i not in id:
+                    need_id.append(i)
+            need_id = list(map(lambda x: x + 1, need_id))
+            if len(need_id) != 0:
+                return render_template('add_text_question.html', img=name_img, ln=len(name_img), mas=need_id)
+            else:
+                pass
+    return render_template('add_text_question.html', img=name_img, ln=len(name_img), mas=[])
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    return render_template('test.html')
 
 
 @login_manager.user_loader
